ab5503422d51b9a35276568e84bbae2749abf1af
João Pedro Zang Gomes <joaozanggomes@gmail.com>
2022-08-05 23:24:25 -0300

    :construction: Place Order

--- future-eats-5/src/Hooks/useForm.js
+++ future-eats-5/src/Hooks/useForm.js
@@ -6,11 +6,20 @@ export const useForm = (initialState) => {
     const onChange = (event) => {
         const { name, value } = event.target;
         setForm({...form, [name]: value});
+        console.log(event.target.value)
+    }
+
+
+    const onChangeRadio = (event) => {
+        const { name, value } = event.target;
+        setForm({ ...form, [name]: value });
+        console.log(initialState)
+
     }
 
     const cleanFields = () => {
         setForm(initialState)
     }
 
-    return { form, setForm, onChange, cleanFields }
+    return { form, setForm, onChange, cleanFields, onChangeRadio }
 }
--- future-eats-5/src/Pages/CartScreen/styled.js
+++ future-eats-5/src/Pages/CartScreen/styled.js
@@ -33,3 +33,53 @@ justify-content: center;
 export const RestaurantName = styled.p`
   color: #5cb646;
 `;
+
+export const DivValue = styled.div`
+width: 100%;
+padding: 16px;
+padding-bottom: 0px;
+flex-direction: column;
+`;
+export const Shipping = styled.p `
+display: flex;
+justify-content: flex-end;
+align-items: center;
+`
+export const DivSubTotal = styled.div`
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: space-between;
+`
+
+export const PSubTotal = styled.p`
+`
+export const ValueTotal = styled.div`
+  color: #5cb646;
+`;
+export const DivPayment = styled.div`
+width: 100%;
+padding: 16px;
+padding-top: 24px;
+padding-bottom: 4px;
+display: flex;
+flex-direction: column;
+gap: 8px;
+`
+export const PayTitle = styled.div `
+
+`
+export const StyleLine = styled.div`
+width: 100%;
+height: 1px;
+background-color: black;
+`
+export const DivInput = styled.div`
+display: flex;
+align-items: center;
+gap: 8px;
+`
+export const Label = styled.label`
+`
+export const InputStyle = styled.input `
+`
